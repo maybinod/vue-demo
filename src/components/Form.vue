877ca1f75bc56984173b5bd307300a680d5c33ce
binod shrestha <maybinod09@gmail.com>
2022-03-04 17:16:48 +0545

    Initital commit

---
+++ src/components/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form>
+      <label>Name</label>
+      <input v-model="form.name">
+
+      <button @click.prevent="onSubmitClick">Submit</button>
+      <slot name="button"></slot>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+
+  data () {
+    return {
+      form: {
+        name: ''
+      }
+    }
+  },
+
+  methods: {
+    onSubmitClick () {
+      this.$emit('form:submit', this.form.name)
+    }
+  },
+
+  props: {
+    email: {
+      require: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <h2>Contact Us Page</h2>
+
+    <Form @form:submit="onFormSubmit" :email="'testEmail'">
+      <template #button>
+        <button>Test</button>
+      </template>
+    </Form>
+  </div>
+</template>
+
+<script>
+import Form from '@/components/Form'
+import axios  from 'axios'
+
+export default {
+  components: {
+    Form
+  },
+
+  data () {
+    return {
+      email: ''
+    }
+  },
+
+  methods: {
+    onFormSubmit (data) {
+      axios.post('http://saveForm.com/api/store', data)
+          .then(() => {
+      })
+    }
+  },
+
+  name: 'ContactUs'
+}
+</script>
+
+<style>
+</style>
